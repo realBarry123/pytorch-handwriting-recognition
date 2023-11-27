ba350f27ea5fca5c540bd4f559e9b61dd52423a4
realBarry123 <barryyu28@gmail.com>
2023-11-26 18:05:06 -0800

    Annotate class Net

--- model.py
+++ model.py
@@ -12,27 +12,28 @@ class Net(nn.Module):  # inherits nn.Module (base class for all nns)
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5)
         self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        # dropout: chops random parts of the neural network to make it more adaptable & prevent overfitting
 
         self.fc1 = nn.Linear(in_features=320, out_features=50)
         self.fc2 = nn.Linear(in_features=50, out_features=10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(x)
 
-        x = self.conv2(x)
-        x = self.conv2_drop(x)
-        x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(x)
+        print(x.size())  # x is a 28 by 28 image
+        x = self.conv1(x)  # run through convolution layer 1
+        x = F.max_pool2d(x, kernel_size=2)  # max pooling
+        x = F.relu(x)  # apply squishification
 
-        x = x.view(-1, 320)
+        x = self.conv2(x)  # run through convolution layer 2
+        x = self.conv2_drop(x)  # dropout
+        x = F.max_pool2d(x, kernel_size=2)  # max pooling
+        x = F.relu(x)  # squishification
 
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
+        x = x.view(-1, 320)  # flatten tensor
 
-        x = self.fc2(x)
-        x = F.log_softmax(x, dim=1)
+        x = self.fc1(x)  # run through linear layer 1
+        x = F.relu(x)  # squishification
+        x = F.dropout(x, training=self.training)  # dropout
+
+        x = self.fc2(x)  # run through linear layer 2
+        x = F.log_softmax(x, dim=1)  # squishification
         return x
